3133b469908091fe2212d7c2a01dd3e93c1b9d59
EasonC13 <gtes9950406@gmail.com>
2022-09-09 10:20:32 +0800

    fix

--- pages/starter/deny.vue
+++ pages/starter/deny.vue
@@ -22,7 +22,7 @@ export default {
   mounted() {},
   methods: {
     next() {
-      window.location = "https://line.me/R/oaMessage/@956unykg/?";();
+      window.location = "https://line.me/R/oaMessage/@956unykg/?";
     },
     back() {
       this.$router.push({
